522cb231876e4392acd8dd4c4c66643b7a0a7360
neclor <81529592+neclor@users.noreply.github.com>
2022-06-11 20:14:35 +0500

    Add files via upload

---
+++ Thermometer.ino
@@ -0,0 +1,116 @@
+#include <max6675.h>
+#include <RTC.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <LiquidCrystal_I2C.h>
+
+int thermoDO = 2; 
+int thermoCS = 3;
+int thermoCLK = 4;
+
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+int vccPin = 5;
+int gndPin = 6;
+
+static DS1307 RTC;
+
+LiquidCrystal_I2C lcd(0x3e, 16, 2);
+
+uint8_t degree[8]  = {140,146,146,140,128,128,128,128};
+
+const int chipSelect = 10;
+
+int sdcard = 0;
+
+char buf[30];
+
+void PrintC(int year, int month, int day, int hours, int minutes, int seconds, float temp, bool C){
+
+  int n = sprintf(buf, "%03d.%01d", (int)temp, (int)(temp*10)%10);
+  
+  if (C){
+    n += sprintf(buf + n, "°C");
+  }
+  
+  sprintf(buf + n, " %02d:%02d:%02d %02d/%02d/%04d", hours, minutes, seconds, day, month, year); 
+}
+void setup() { 
+  Serial.begin(9600);
+
+  pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
+  pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
+  
+  delay(500);
+
+  RTC.begin();
+   
+  lcd.begin();
+  lcd.createChar(0, degree);
+
+  while (!Serial) {
+    ;
+  }
+  if (SD.begin(chipSelect)) {
+    Serial.println("SD card initialized");
+    sdcard = 1;
+  }        
+  else{
+    Serial.println("SD card initialization error, recording will not be performed");
+  }
+  
+}
+
+void loop() { 
+  int year, month, day, hours, minutes, seconds;
+  do { 
+  year = RTC.getYear();
+  month = RTC.getMonth();
+  day = RTC.getDay();
+  hours = RTC.getHours();
+  minutes = RTC.getMinutes();
+  seconds = RTC.getSeconds();
+  }   
+  while (RTC.getMinutes() != minutes);
+
+  float temp = thermocouple.readCelsius();
+
+  lcd.setCursor(0,0);
+  sprintf(buf, "%02d/%02d/%04d", day, month, year);
+  lcd.print(buf);
+  lcd.setCursor(0,1);
+  sprintf(buf, "%03d.%01d", (int)temp, (int)(temp*10)%10);
+  lcd.print(buf);
+  lcd.setCursor(5,1);
+  lcd.print(char(0));
+  lcd.print("C");
+  lcd.setCursor(8,1);
+  sprintf(buf, "%02d:%02d:%02d", hours, minutes, seconds);
+  lcd.print(buf);
+
+  PrintC(year, month, day, hours, minutes, seconds, temp, true);
+  Serial.write(buf);
+
+  if (sdcard == 1) {
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    
+    if (dataFile) {
+      PrintC(year, month, day, hours, minutes, seconds, temp, false);
+      dataFile.write(buf);
+      dataFile.println();
+      dataFile.close();
+    }
+    else {
+      Serial.print(" Error writing to SD card");
+      lcd.setCursor(11,0);
+      lcd.print("ERROR");  
+    }
+  }
+
+  Serial.println();
+  
+  delay(3000);
+
+  lcd.setCursor(11,0);
+  lcd.print("     ");
+}
